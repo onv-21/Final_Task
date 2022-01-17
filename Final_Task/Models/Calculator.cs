f33876175b957a04f033b62df06e0b269f470709
Revit-PC1 <Revit-PC1@PC1>
2022-01-17 10:18:06 +0800

    Добавьте файлы проекта.

---
+++ Final_Task/Models/Calculator.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Task.Models
+{
+    public class Calculator
+    {
+        private string result;
+
+        # region Конструкторы
+
+        public Calculator(string firstOperation, string secondOperation, string operation)
+        {
+            FirstOperation = firstOperation;
+            SecondOperation = secondOperation;
+            Operation = operation;
+            result = string.Empty;
+        }
+
+        public Calculator(string firstOperation, string operation)
+        {
+            FirstOperation = firstOperation;
+            SecondOperation = string.Empty;
+            Operation = operation;
+            result = string.Empty;
+        }
+
+        public Calculator()
+        {
+            FirstOperation = string.Empty;
+            SecondOperation = string.Empty;
+            Operation = string.Empty;
+            result = string.Empty;
+        }
+        #endregion
+
+
+        # region Свойства
+
+        public string FirstOperation { get; set; }
+        public string SecondOperation { get; set; }
+        public string Operation { get; set; }
+        public string Result { get { return result; } }
+        #endregion
+
+
+        # region Методы
+        public void CalculateResult()
+        {
+            double Factorial(double firstOperation)
+            {
+                if (firstOperation == 1) return 1;
+
+                return firstOperation * Factorial((firstOperation) - 1);
+            }
+            switch (Operation)
+            {
+                case ("+"):
+                    result = (Convert.ToDouble(FirstOperation) + Convert.ToDouble(SecondOperation)).ToString();
+                    break;
+
+                case ("-"):
+                    result = (Convert.ToDouble(FirstOperation) - Convert.ToDouble(SecondOperation)).ToString();
+                    break;
+
+                case ("x"):
+                    result = (Convert.ToDouble(FirstOperation) * Convert.ToDouble(SecondOperation)).ToString();
+                    break;
+
+                case ("/"):
+                    result = (Convert.ToDouble(FirstOperation) / Convert.ToDouble(SecondOperation)).ToString();
+                    break;
+
+                case ("%"):
+                    result = (Convert.ToDouble(FirstOperation)* Convert.ToDouble(SecondOperation)/ 100).ToString();
+                    break;
+
+                case ("√х"):
+                    result = Convert.ToDouble(FirstOperation) < 0 ? "Ошибка" : Math.Sqrt(Convert.ToDouble(SecondOperation)).ToString();
+                    break;
+
+                case ("х²"):
+                    result = Math.Pow(Convert.ToDouble(FirstOperation), 2).ToString();
+                    break;
+
+                case ("1/x"):
+                    result = Convert.ToDouble(FirstOperation) == 0 ? "Ошибка" : (1 / Convert.ToDouble(FirstOperation)).ToString();
+                    break;
+
+                case ("cos"):
+                    result = Math.Cos(Convert.ToDouble(SecondOperation)).ToString();
+                    break;
+
+                case ("sin"):
+                    result = Math.Sin(Convert.ToDouble(SecondOperation)).ToString();
+                    break;
+
+                case ("tan"):
+                    result = Math.Tan(Convert.ToDouble(SecondOperation)).ToString();
+                    break;
+
+                case ("exp"):
+                    result = Math.Exp(Convert.ToDouble(SecondOperation)).ToString();
+                    break;
+
+                case ("lg"):
+                    result = Math.Log10(Convert.ToDouble(SecondOperation)).ToString();
+                    break;
+
+                case ("In"):
+                    result = Math.Log(Convert.ToDouble(SecondOperation)).ToString();
+                    break;
+
+                case ("xᵞ"):
+                    result = Math.Pow(Convert.ToDouble(FirstOperation), Convert.ToDouble(SecondOperation)).ToString();
+                    break;
+
+                case ("10ᵡ"):
+                    result = Math.Pow(10,Convert.ToDouble(SecondOperation)).ToString();
+                    break;
+
+                case ("n!"):
+                   
+                    result = Factorial(Convert.ToDouble(SecondOperation)).ToString();
+                    break;
+
+            }
+        }
+        #endregion
+    }
+}
---
+++ Final_Task/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,266 @@
+﻿using Final_Task.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Final_Task.ViewModels
+{
+    internal class MainWindowViewModel : INotifyPropertyChanged
+    {
+        #region Событие изменение свойств
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+        #endregion
+
+        //Коллекция для сохранения вычислений в файл
+        private Collection<string> computationHistory;
+
+        #region Поля
+
+        private Calculator calculation;
+
+        private string lastOperation;
+        private bool newDisplayRequired = false;
+        private string fullExpression;
+        private string display;
+        #endregion
+
+        #region Конструктор
+
+        public MainWindowViewModel()
+        {
+            calculation = new Calculator();
+
+            //Коллекция для сохранения вычислений в файл
+            computationHistory = new Collection<string>();
+
+            //Поля
+            display = "0";
+            FirstOperation = string.Empty;
+            SecondOperation = string.Empty;
+            Operation = string.Empty;
+            lastOperation = string.Empty;
+            fullExpression = string.Empty;
+
+            //Команды
+            CopyCommand = new RelayCommand(OnCopyCommandExecute, CanCopyCommandExecuted);
+            OperationButtonPressCommand = new RelayCommand(OnOperationButtonPressCommandExecute, CanOperationButtonPressCommandExecuted);
+            SingularOperationButtonPressCommand = new RelayCommand(OnSingularOperationButtonPressCommandExecute, CanSingularOperationButtonPressCommandExecuted);
+            DigitButtonPressCommand = new RelayCommand(OnDigitButtonPressCommandExecute, CanDigitButtonPressCommandExecuted);
+        }
+        #endregion
+
+        #region Свойства
+
+       
+        public string FirstOperation
+        {
+            get => calculation.FirstOperation;
+            set { calculation.FirstOperation = value; }
+        }
+
+       
+        public string SecondOperation
+        {
+            get => calculation.SecondOperation;
+            set { calculation.SecondOperation = value; }
+        }
+
+        
+        public string Operation
+        {
+            get => calculation.Operation;
+            set { calculation.Operation = value; }
+        }
+
+        
+        public string LastOperation
+        {
+            get => lastOperation;
+            set { lastOperation = value; }
+        }
+
+        
+        public string Result
+        {
+            get => calculation.Result;
+        }
+
+       
+        public string Display
+        {
+            get => display;
+            set => Set(ref display, value);
+        }
+
+       
+        public string FullExpression
+        {
+            get => fullExpression;
+            set => Set(ref fullExpression, value);
+        }
+        #endregion
+
+        #region Команды
+
+        //Копирование результата вычислений с дисплея в буфер обмена
+        public ICommand CopyCommand { get; }
+
+        private void OnCopyCommandExecute(object p)
+        {
+            Clipboard.SetText(Display);
+        }
+
+        private bool CanCopyCommandExecuted(object p) => true;
+
+        //Команда обработчик нажатия на кнопки оперций с двумя операциями
+        public ICommand OperationButtonPressCommand { get; }
+
+        private void OnOperationButtonPressCommandExecute(object p)
+        {
+            if (FirstOperation == string.Empty || LastOperation == "=")
+            {
+                FirstOperation = display;
+                LastOperation = p.ToString();
+            }
+            else
+            {
+                SecondOperation = display;
+                Operation = lastOperation;
+                calculation.CalculateResult();
+
+                if (Operation == "/" && SecondOperation == "0")
+                {
+                    Display = "Ошибка";
+                    newDisplayRequired = true;
+                    return;
+                }
+                else
+                {
+                    FullExpression = Math.Round(Convert.ToDouble(FirstOperation), 10) + Operation
+                        + Math.Round(Convert.ToDouble(SecondOperation), 10) + "="
+                        + Math.Round(Convert.ToDouble(Result), 10);
+
+                    computationHistory.Add(FullExpression);
+                    LastOperation = p.ToString();
+                    Display = Result;
+                    FirstOperation = display;
+                }
+            }
+            newDisplayRequired = true;
+        }
+
+        private bool CanOperationButtonPressCommandExecuted(object p) => true;
+
+        /// <summary>Команда обработчик нажатия на кнопки оперций с одним операндом</summary>
+        public ICommand SingularOperationButtonPressCommand { get; }
+
+        private void OnSingularOperationButtonPressCommandExecute(object p)
+        {
+            FirstOperation = Display;
+            Operation = p.ToString();
+            calculation.CalculateResult();
+
+            if (Operation == "1/x" && FirstOperation == "0" || Operation == "√х" && Convert.ToDouble(FirstOperation) < 0)
+            {
+                Display = "Ошибка";
+                newDisplayRequired = true;
+                return;
+            }
+            else
+            {
+                FullExpression = Operation + "(" + Math.Round(Convert.ToDouble(FirstOperation), 10) + ")="
+                    + Math.Round(Convert.ToDouble(Result), 10);
+
+                computationHistory.Add(FullExpression);
+                LastOperation = "=";
+                Display = Result;
+                FirstOperation = display;
+                newDisplayRequired = true;
+            }
+        }
+
+        private bool CanSingularOperationButtonPressCommandExecuted(object p) => true;
+
+        /// <summary>Команда обработчик нажатия на цифровые кнопки</summary>
+        public ICommand DigitButtonPressCommand { get; }
+
+        private void OnDigitButtonPressCommandExecute(object p)
+        {
+            switch (p)
+            {
+                case "C":
+                    Display = "0";
+                    FirstOperation = string.Empty;
+                    SecondOperation = string.Empty;
+                    Operation = string.Empty;
+                    LastOperation = string.Empty;
+                    FullExpression = string.Empty;
+                    break;
+
+                case "←":
+                    Display = display.Length > 1 ? display.Substring(0, display.Length - 1) : "0";
+                    break;
+                case "+/-":
+                    Display = display.Contains("-") ? display.Remove(display.IndexOf("-"), 1) : "-" + display;
+                    break;
+                case ",":
+                    if (newDisplayRequired)
+                    {
+                        Display = "0,";
+                    }
+                    else
+                    {
+                        if (!display.Contains(","))
+                        {
+                            Display = display + ",";
+                        }
+                    }
+                    break;
+                default:
+                    Display = display == "0" || newDisplayRequired ? p.ToString() : display + p.ToString();
+                    break;
+
+                case "(":
+                    Display = display + "(";
+                    break;
+
+                case ")":
+                    Display = display + ")";
+                    break;
+
+                case "π":
+                    Display = display + Math.PI;
+                    break;
+
+                case "e":
+                    Display = display + Math.E;
+                    break;
+            }
+            newDisplayRequired = false;
+        }
+
+        private bool CanDigitButtonPressCommandExecuted(object p) => true;
+        #endregion
+    }
+}
